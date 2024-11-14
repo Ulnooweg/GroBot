031586399be8f9307c6d19c7fe3aaf180e3c200d
TNarakol-UEC <97195136+TNarakol-UEC@users.noreply.github.com>
2024-11-14 16:47:33 -0400

    Implemented csv file containing machine info (software version and serial number) and methods to read them

--- Software/code/config.py
+++ Software/code/config.py
@@ -9,18 +9,20 @@
 #GroBot
 #Code: config
 #Version: 1.1
-#Description: Code to read and manipulate configuration file grobot_cfg.ini
+#Description: Code to read and manipulate configuration file grobot_cfg.ini, read system info from ulnoowegdat csv
 #Function: get_plant _settings(), read config and return plant settings as a tuple of data
 #          read_config(), read config.ini and return a dictionary value of all data
 #          update_config(section, parameter, value), write "value" to config file under "parameter" parameter in "section" section
+#          readcsv(), read data from ulnoowegdat and return it as a list
 #Input: update_config requires section and parameter in strings and value in any form convertible to strings. 
-#Output: returns dictionary variable in string for read_config() and in integer for get_plant_settings(); output data to file for update_config(...)
+#Output: returns dictionary variable in string for read_config() and in integer for get_plant_settings(); output data to file for update_config(...); output list of data for readcsv
 #Error Handling: NONE
 #
 ########################################
 
 #Import important modules
 import configparser
+import csv
 
 config = configparser.ConfigParser()
 config.optionxform = str #Force keep case-sensititvity in config file
@@ -48,3 +50,12 @@ def update_config(section, parameter, value): #Define function to write variable
     config[section][parameter] = str(value)
     with open('/mnt/grobotextdat/userdata/grobot_cfg.ini', 'w') as configfile:
         config.write(configfile)
+
+def readcsv(): #define a function to read csv file
+    csvdata = [] #define an empty list
+    with open('/mnt/grobotextdat/userdata/ulnoowegdat', 'r') as csvfile: #open the csv file as csvfile object
+        csvraw = csv.reader(csvfile) #read csvfile into csvraw using reader class from csv library
+        for row in csvraw: #iterate through each row in cswraw
+            csvdata.append(row) #for each row, append the data to the list
+
+    return csvdata #return the csv data in list form
